5d3b1e0c458f7ca54f5fee6fc62e569ee84dfcda
nasioutz <nasioutz@csd.auth.gr>
2019-07-01 14:16:27 +0300

    Table Script Update

--- examples/dch/table_script.py
+++ examples/dch/table_script.py
@@ -1,23 +1,42 @@
 import numpy as np
 from os.path import join
 import csv
+import pickle
 
 snapshots = ['2019_6_9_12_33_4']
+table = 'results_table.csv'
 
-for snapshot in snapshots:
+def add_to_table(snapshots, table='default_table.csv'):
 
-    inf = open(join(snapshot, 'log.txt'), 'r')
-    args = eval(inf.readline()[1:-2])
-    inf.close()
+    for snapshot in snapshots:
 
-    r = np.load(join(snapshot, "models", "recuring_results.npy"))
-    r = r[:, :, 0]
-    r_max = np.max(r, 1)
+        r = np.load(join(snapshot, "models", "recuring_results.npy"))
+        r = r[:, :, 0]
+        r_max = np.max(r, 1)
 
-    st_deviation = np.std(r_max, ddof=1)
-    mean = np.mean(r_max)
+        st_deviation = np.std(r_max, ddof=1)
+        mean = np.mean(r_max)
 
-    with open(join("results_table.csv"), "a", newline='') as myFile:
-        writer = csv.writer(myFile)
-        writer.writerow([args['dataset'],args['output_dim'],args['regularizer'],args['regularization_factor'],mean,st_deviation])
+        try:
+            with open(join(snapshot,"args.file"), "rb") as f:
+                args = pickle.load(f)
 
+            with open(join(table), "a", newline='') as myFile:
+                writer = csv.writer(myFile)
+                writer.writerow(
+                    [args.dataset, args.output_dim, args.regularizer, args.regularization_factor, mean, st_deviation])
+        except:
+            inf = open(join(snapshot, 'log.txt'), 'r')
+            args = eval(inf.readline()[1:-2])
+            inf.close()
+
+            with open(join(table), "a", newline='') as myFile:
+                writer = csv.writer(myFile)
+                writer.writerow(
+                    [args['dataset'], args['output_dim'], args['regularizer'], args['regularization_factor'], mean, st_deviation])
+
+
+
+if __name__ == "__main__":
+
+    add_to_table(snapshots, table)
